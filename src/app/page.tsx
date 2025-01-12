b653a3c23980ffb5e5c75ce27e244493711e0856
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-12 17:09:03 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import Hero from "./components/hero";
+import Navbar from "./components/navbar"
+import Chat from "./../chat"
+
+export default function Home() {
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+     <Chat/>
+    </>
+  );
+}
